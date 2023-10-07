9a7f3a7e6342e3cb943f9405e0b543b4ef194a61
David Schubert <github@davidschubert.com>
2023-10-07 19:08:21 +0200

    reorganized app

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,42 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import PageHome from '@/components/PageHome.vue'
-import PageThreadShow from '@/components/PageThreadShow.vue'
-import PageNotFound from '@/components/PageNotFound.vue'
+import Home from '@/pages/Home.vue'
+import ThreadShow from '@/pages/ThreadShow.vue'
+import NotFound from '@/pages/NotFound.vue'
+import sourceData from '@/data.json'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: PageHome
+    component: Home
   },
   {
     path: '/thread/:id',
     name: 'ThreadShow',
-    component: PageThreadShow,
-    props: true
+    component: ThreadShow,
+    props: true,
+    beforeEnter(to, from, next) {
+      // check if thread exists
+      const threadExists = sourceData.threads.find((thread) => thread.id === to.params.id)
+      // if exists continue
+      if (threadExists) {
+        return next()
+      } else {
+        // if not exists redirect to not found
+        next({
+          name: 'NotFound',
+          params: { pathMatch: to.path.substring(1).split('/') },
+          // preserve existing query and hash
+          query: to.query,
+          hash: to.hash
+        })
+      }
+    }
   },
   {
     path: '/:pathMatch(.*)*',
     name: 'NotFound',
-    component: PageNotFound
+    component: NotFound
   }
 ]
 
